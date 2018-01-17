b1b87b9b56194821c205050322f218c102c739d5
Kevin Busch <busch.kevin@gmx.de>
2018-01-17 21:34:31 +0100

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by busch on 17.01.2018.
+ */
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
